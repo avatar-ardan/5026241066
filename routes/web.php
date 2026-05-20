b589a141d2c03b3ec5e34a956ea28d52d2c85b7d
avatar-ardan <avatar.ardan@gmail.com>
2026-05-20 12:20:10 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DosenController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\BlogController ;
+use App\Http\Controllers\PegawaiDBController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -39,10 +40,13 @@ Route::get('pertemuan5', [PageController::class, 'pertemuan5']);
 Route::get('tugasLinktree', [PageController::class, 'tugasLinktree']);
 Route::get('makeover-ig', [PageController::class, 'makeoverig']);
 
-Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/pegawailama/{nama}', [PegawaiController::class, 'index']);
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
 //blog
 Route::get('/blog', [BlogController::class, 'home']);
 Route::get('/blog/tentang', [BlogController::class, 'tentang']);
 Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+//route CRUD
+Route::get('/pegawai',[PegawaiDBController::class, 'index']);
